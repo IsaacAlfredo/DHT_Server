ae4366538c76777a8e0498ea94e0408f03033d7f
Isaac Alfredo <isaacfreitas23@gmail.com>
2023-10-03 01:02:11 -0300

    next

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ server.use(require("express").static("public"));
 let temperature = 0;
 let humidity = 0;
 
-server.get("/update", (req, res) => {
+server.get("/update", (req, res, next) => {
   const params = req.query;
   temperature = params.temperature;
   humidity = params.humidity;
@@ -15,13 +15,13 @@ server.get("/update", (req, res) => {
   res.status(200).send("OK");
 });
 
-server.get("/temperature", (req, res) => {
+server.get("/temperature", (req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   next();
   res.status(200).send(temperature.toString());
 });
 
-server.get("/humidity", (req, res) => {
+server.get("/humidity", (req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   next();
   res.status(200).send(humidity.toString());
