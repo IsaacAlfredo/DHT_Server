1f5f22e2dc8e1ec95e62faaa501126c098470c7a
Isaac Alfredo <isaacfreitas23@gmail.com>
2023-10-03 15:44:52 -0300

    hist

--- server.js
+++ server.js
@@ -5,11 +5,19 @@ server.use(require("express").static("public"));
 
 let temperature = 0;
 let humidity = 0;
+let temperatureHist = [];
+let humidityHist = [];
 
 server.get("/update", (req, res) => {
   const params = req.query;
   temperature = params.temperature;
   humidity = params.humidity;
+  if (temperatureHist.length > 9) {
+    temperatureHist.shift();
+    humidityHist.shift();
+  }
+  temperatureHist.push(temperature);
+  humidityHist.push(humidity);
   res.header("Access-Control-Allow-Origin", "*");
   res.status(200).send("OK");
 });
@@ -22,6 +30,14 @@ server.get("/temperature", (req, res) => {
 server.get("/humidity", (req, res) => {
   res.header("Access-Control-Allow-Origin", "*");
   res.status(200).send(humidity.toString());
+});
+
+server.get("/hist", (req, res) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.status(200).json({
+    temperatureHist,
+    humidityHist,
+  });
 });
 
 server.listen(3000, () => {
